77635a5523034e42948c60f52ebe37ef22ff9114
SAMIKSHAABAL <77974265+SAMIKSHAABAL@users.noreply.github.com>
2021-09-01 18:32:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+var SpeechRecognition= window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition();
+function start(){
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+}
+recognition.onresult=function(event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+ console.log(Content);
+ if(Content=="take my selfie")
+
+ 
+ {
+     console.log("taking selfie");
+    speak();}
+
+}
+ 
+function speak(){
+ var synth= window.speechSynthesis;
+ speak_data= "Taing Selfie in 5 seconds";
+ var utterThis= new SpeechSynthesisUtterance(speak_data);
+ synth.speak(utterThis);
+ Webcam.attach(camera);
+ setTimeout(function(){
+    take_snapshot();
+    save();
+ },5000);
+}
+camera= document.getElementById("camera");
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:'jpeg',
+    jpeg_quality:90
+});
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="Selfie" src="'+data_uri+'">';        
+    });
+}
+function save() {
+ link=document.getElementById("link");
+ imag=document.getElementById("Selfie").src;
+link.href=imag;
+link.click();
+}
